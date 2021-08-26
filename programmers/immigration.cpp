5d22c1bb82b797fa9e2dc10ca5a12802b9429f5c
literate-t <gaekoon@gmail.com>
2021-08-26 23:23:28 +0900

    프로그래머스/입국심사-진행 중

---
+++ programmers/immigration.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+long long solution(int n, vector<int> times) 
+{
+    long long answer = 0;
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(6, { 7, 10 }) << endl;
+}
