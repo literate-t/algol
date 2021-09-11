1199a2653020cebc9529815378eed29a2b9c521d
literate-t <gaekoon@gmail.com>
2021-09-11 21:37:38 +0900

    fixed hacker/Extra long factorial

--- hacker/extra_long_factorials.cpp
+++ hacker/extra_long_factorials.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main()
+
+void extraLongFactorials(int n)
 {
-	int N = 25;	
 	vector<int> answer;
 	answer.push_back(1);
-	// number to multifly
-	for (int i = 2; i <= N; ++i)
+	for (int i = 2; i <= n; ++i)
 	{
-		for (auto it = answer.begin(); it != answer.end(); ++it)
-			*it *= i;
-		for (size_t j = 0; j < answer.size(); ++j)
+		int carry = 0;
+		for (int j = 0; j < answer.size(); ++j)
 		{
-			auto a = answer[j];
-			if (a < 10) continue;
-
-			if (answer.size() == j + 1)
-				answer.push_back(0);// increae digit
-			answer[j] = a % 10;
-			answer[j + 1] += a / 10;
+			auto res = i * answer[j] + carry;
+			if (res < 10)
+			{
+				answer[j] = res;
+				carry = 0;
+				continue;
+			}
+			else if (j + 1 == answer.size())
+				answer.push_back(0);
+			answer[j] = res % 10;
+			carry = res / 10;
 		}
 	}
 	for (auto it = answer.rbegin(); it != answer.rend(); ++it)
-		cout << *it;
+		printf("%d", *it);
+}
+
+int main()
+{
+	extraLongFactorials(25);
 }
